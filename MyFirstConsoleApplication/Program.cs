d79e43b1995bc8e3bc7bbdadf391eedf80b019b8
Efrain Cataño <efrain.catano97@gmail.com>
2023-04-21 23:39:21 -0400

    last update

--- MyFirstConsoleApplication/Program.cs
+++ MyFirstConsoleApplication/Program.cs
@@ -26,8 +26,8 @@ heightString = Console.ReadLine();
 height = double.Parse(heightString);
 woodLength = 2 * (width + height) * 3.25;
 glassArea = 2 * (width * height);
-Console.WriteLine("The length of the wood needed is " + woodLength + " feet.");
-Console.WriteLine("The area of the glass needed is " + glassArea + " square metres.");
+Console.WriteLine ( "The length of the wood is " + woodLength + " feet" ) ;
+Console.WriteLine( "The area of the glass is " + glassArea + " square metres" ) ;
 
 
 
